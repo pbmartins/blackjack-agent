14bf821fd4fa57c7490a389cebfd020e7dc6d1da
Pedro Martins <pbmartins@ua.pt>
2016-10-21 19:54:06 +0100

    Added base files

---
+++ casino.py
@@ -0,0 +1,15 @@
+from game import Game
+from player import Player
+from randomplayer import RandomPlayer
+
+if __name__ == '__main__':
+
+    players = [Player("Human",100)]
+
+    for i in range(100):
+        print(players)
+        g = Game(players, min_bet=1, max_bet=5) 
+        #g = Game(players, debug=True)
+        g.run()
+
+    print("OVERALL: ", players)
